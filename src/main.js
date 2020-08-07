4afd414d81a55e36b9d8e467772785d06d812182
Anastacia Grigoryeva <36881990+Asya-brisk@users.noreply.github.com>
2020-08-08 00:19:08 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+import {createSiteMenuTemplate} from "./view/site-menu.js";
+import {createFilterTemplate} from "./view/filter.js";
+import {createTaskTemplate} from "./view/task.js";
+import {createTaskEditTemplate} from "./view/task-edit.js";
+import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
+import {createBoardTemplate} from "./view/board.js";
+
+const TASK_COUNT = 3;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
